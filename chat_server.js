c7cea26937a1834cd17119921b5fefa902752a87
Gillian Laming <gdlaming@wustl.edu>
2018-11-10 11:49:57 -0600

    hi

--- chat_server.js
+++ chat_server.js
@@ -88,7 +88,7 @@ io.on('connection', function (socket) {
 		con.query(qry, name)
 			.on('data', function(result){
 				user1 = result.user;
-				socket.emit("display_users",user1) // send users to client MAYBE CHANGE TO SOCKET.EMIT (?)
+				io.sockets.emit("display_users",user1) // send users to client MAYBE CHANGE TO SOCKET.EMIT (?)
 			})
 			.on('error', console.error); 	
 	});
@@ -123,6 +123,7 @@ io.on('connection', function (socket) {
 			.on('error', console.error); 
 		console.log('removed '+socket.username+' from room');
 	});
+
 });
 
 server.listen(3456);
